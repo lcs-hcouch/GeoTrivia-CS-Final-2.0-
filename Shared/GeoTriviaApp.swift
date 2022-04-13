519c77c2c563de609118d71e71c0922274d7232d
Hayden Couch <hcouch@lcs.on.ca>
2022-04-12 21:25:16 -0400

    Initial Commit

---
+++ Shared/GeoTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoTriviaApp.swift
+//  Shared
+//
+//  Created by Hayden Couch on 2022-04-12.
+//
+
+import SwiftUI
+
+@main
+struct GeoTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
